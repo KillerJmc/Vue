387c1a84c7e89445f98522f2bb3ae74c8e98ed87
Jmc <1934497212@qq.com>
2022-02-09 16:17:28 +0800

    axios: CORS.

--- axios/src/network/request.js
+++ axios/src/network/request.js
@@ -6,13 +6,30 @@ import axios from "axios";
 // 默认配置
 let instance = axios.create({
   baseURL: 'https://getman.cn/mock',
+  // 接受跨域的Credentials（证书：如请求头，Cookie等）
+  withCredentials: true,
   timeout: 5000
 })
 
-// 拦截器，一般有用于拦截和处理一些请求
+// 请求拦截器
+instance.interceptors.request.use(config => {
+  // 打印发送的url
+  // 构造一个对象并转换为json字符串表示
+  console.log("request " + JSON.stringify({
+    url: config.url,
+    data: config.data
+  }, null, 4))
+  return config
+})
+
+// 回复拦截器
 instance.interceptors.response.use(config => {
+  console.log("response " + JSON.stringify(config.data, null, 4))
   // 让用户不需要调用.data
-  return config.data;
+  return config.data
+}, e => {
+  // 打印错误
+  alert(e)
 })
 
 export default instance
